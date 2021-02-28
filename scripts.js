104134cbfbd7b9f5c77897fa6a5e9108c4ae7df8
bimonestle <bimonestle@yahoo.co.id>
2021-02-28 12:57:31 +0700

    working with jquery not() element selection

--- scripts.js
+++ scripts.js
@@ -4,4 +4,7 @@ jQuery('.widget-title:eq(0)').css({ background: 'green' });
 
 // gt() selects element after the index/argument given.
 // gt(2) means select the particular elements after the index 2 / third element
-jQuery('.widget-title:gt(2)').css({ background: 'green' });
+jQuery('.widget-title:gt(2)').css({ background: 'blue' });
+
+// not() removes an item from the selection
+jQuery('.widget:not(.widget_categories) .widget-title').css({ background: 'red' });
