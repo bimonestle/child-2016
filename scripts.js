281c0511e003d04b11faf3ca4e8966188434ab94
bimonestle <bimonestle@yahoo.co.id>
2021-02-28 13:30:37 +0700

    learn modify content

--- scripts.js
+++ scripts.js
@@ -7,4 +7,14 @@ jQuery('.widget-title:eq(0)').css({ background: 'green' });
 jQuery('.widget-title:gt(2)').css({ background: 'blue' });
 
 // not() removes an item from the selection
-jQuery('.widget:not(.widget_categories) .widget-title').css({ background: 'red' });
+jQuery('.widget:not(.widget_categories) .widget-text').css({ background: 'red' });
+
+// traversing the DOM
+jQuery('aside').children().find('h2').css({ background: 'brown' });
+
+// traversing the DOM
+jQuery('aside').children().has('.widget-title').css({ background: 'cadetblue' });
+
+// Modifying Content
+jQuery('#masthead .site-description').html( 'Read me on <a href="http://premium.wpmudev.org/blog/author/danielpataki">WPMU DEV</a> as well' );
+jQuery('#masthead .site-description').text( 'Read me on <a href="http://premium.wpmudev.org/blog/author/danielpataki">WPMU DEV</a> as well' );
